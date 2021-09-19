ab84a3146545cd62c506b7e21a949c9bce7930bd
GManners-Trinity <gmanners@trinity.edu>
2021-09-18 23:55:58 -0500

    Initial Scene Setup

---
+++ Assets/GameManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameManager instance;
+
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(instance);
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+       SceneManager.LoadScene("MainGameScene", LoadSceneMode.Single);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void ViewRules()
+    {
+        SceneManager.LoadScene("RulesMenu", LoadSceneMode.Single);
+    }
+
+    public void ViewCardBacks()
+    {
+        SceneManager.LoadScene("CardBackMenu", LoadSceneMode.Single);
+    }
+
+    public void ViewMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+}
